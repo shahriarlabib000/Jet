92985707ae65ea9b871ea4cc36643cd73283ed1e
shahriarlabib000 <shahriarlabib000@gmail.com>
2024-08-11 10:46:02 +0600

    improvements

--- scenes/jet/jet.gd
+++ scenes/jet/jet.gd
@@ -1,6 +1,6 @@
 extends RigidBody3D
 
-@export var speed:int=200
+@export var speed:int=500
 @export var rollSpeed:int=2000
 @export var turnSpeed:int=1500
 var dir:float=0
@@ -9,7 +9,8 @@ func _ready() -> void:
 	print(get_viewport().size)
 
 func _integrate_forces(state: PhysicsDirectBodyState3D) -> void:
-	state.add_constant_central_force(basis.z * -speed)
+	state.apply_central_force(basis.z * -speed)
+	
 	
 func _physics_process(delta: float) -> void:
 	dir=Input.get_axis("ui_right","ui_left")
@@ -18,4 +19,5 @@ func _physics_process(delta: float) -> void:
 	apply_torque(basis.x * delta * dir * turnSpeed)
 	dir=Input.get_axis("rRight","rLeft")
 	apply_torque(basis.z * delta * dir * rollSpeed)
+	$thirdPerson.global_rotation.z=0
 	
