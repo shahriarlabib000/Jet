3c8e8c87d76f4f0360017e4a0bb75d5b801deab9
shahriarlabib000 <shahriarlabib000@gmail.com>
2024-08-15 13:05:56 +0600

    auto rotation

--- scenes/jet/jet.gd
+++ scenes/jet/jet.gd
@@ -5,6 +5,7 @@ extends RigidBody3D
 @export var turnSpeed:int=1500
 @onready var uiScript=preload("res://scenes/ui/ui.gd")
 var dir:float=0
+var roll:bool
 
 func _ready() -> void:
 	print(get_viewport().size)
@@ -15,16 +16,27 @@ func _integrate_forces(state: PhysicsDirectBodyState3D) -> void:
 	
 func _physics_process(delta: float) -> void:
 	dir=Input.get_axis("ui_right","ui_left")
+	if dir:
+		roll=false
 	apply_torque(basis.y * delta * dir * turnSpeed)
-	if(abs(global_rotation_degrees.z)< 10):
+	if(abs(global_rotation_degrees.z)< 30):
 		apply_torque(basis.z * delta * dir * turnSpeed)
+		
 	
 	dir=Input.get_axis("ui_up","ui_down")
 	apply_torque(basis.x * delta * dir * turnSpeed)
 	
 	dir=Input.get_axis("rRight","rLeft")
 	apply_torque(basis.z * delta * dir * rollSpeed)
+	if dir:
+		roll=true
+		
+	if(global_rotation.z!=0 and !roll):
+		apply_torque(basis.z * -(global_rotation_degrees.z/abs(global_rotation_degrees.z)) * delta * turnSpeed/3)
+	
 	$tppNode.global_rotation.z=0
+	
+	
 	if uiScript.speed<=200:
 		gravity_scale=0.3
 		#apply_central_impulse(basis.z *delta * 90*5* -uiScript.speed)
