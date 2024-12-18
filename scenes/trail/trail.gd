0fc88142d7ee3fc32724a7e9eafb6b6a29219936
shahriarlabib000 <shahriarlabib000@gmail.com>
2024-12-18 07:26:36 +0600

    trail

---
+++ scenes/trail/trail.gd
@@ -0,0 +1,21 @@
+extends MeshInstance3D
+
+@export var target:Node3D
+@export var max_count := 5000
+var points :Array[Vector3] = []
+
+func _ready() -> void:
+	mesh = ImmediateMesh.new()
+
+func _physics_process(_delta: float) -> void:
+	if is_instance_valid(target):
+		if points.size() < max_count:
+			points.push_back(target.global_position)
+		else:
+			points.pop_front()
+		
+		mesh.clear_surfaces()
+		mesh.surface_begin(mesh.PRIMITIVE_LINES)
+		for point:Vector3 in points:
+			mesh.surface_add_vertex(point)
+		mesh.surface_end()
